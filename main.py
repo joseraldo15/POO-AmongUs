de40185fce75832ff2728f717540a56985e9dec6
joseraldo15 <72880099+joseraldo15@users.noreply.github.com>
2020-10-14 14:15:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 class Crewmate:
     
-    def _init_(self, name, color):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.do_task = True
-        self.sabotage = False
-        self.vent = False
+        self.can_do_task = True
+        self.can_sabotage = False
+        self.can_vent = False
     
     def sabotage(self):
-        if self.sabotage:
+        if self.can_sabotage:
             print(f'{self.name} ruined the ship!')
         else:
             print('You are not the impostor, stop playing games!')
     
     def vent(self):
-        if self.vent:
+        if self.can_vent:
             print(f'you vented.')
         else:
             print('why would you do that?')
     
     def do_task(self):
-        if self.sabotage and self.vent:
+        if self.can_sabotage and self.can_vent:
             print('you have to fake your tasks!')
         else:
             print('complete your tasks to win.')
@@ -28,27 +28,40 @@ class Crewmate:
 
 class Impostor:
 
-    def _init_(self, name, color):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.do_task = False
-        self.sabotage = True
-        self.vent = True
+        self.can_do_task = False
+        self.can_sabotage = True
+        self.can_vent = True
     
     def sabotage(self):
-        if self.sabotage:
+        if self.can_sabotage:
             print(f'{self.name} ruined the ship!')
         else:
             print('You are not the impostor, stop playing games!')
     
     def vent(self):
-        if self.vent:
+        if self.can_vent:
             print(f'you vented.')
         else:
             print('why would you do that?')
     
     def do_task(self):
-        if self.sabotage and self.vent:
+        if self.can_sabotage and self.can_vent:
             print('you have to fake your tasks!')
         else:
             print('complete your tasks to win.')
+
+
+t1 = Crewmate('XANDÃO', 'Azul')
+
+t1.sabotage()
+t1.vent()
+t1.do_task()
+
+i1 = Impostor('PAXTEL', 'Verde')
+
+i1.sabotage()
+i1.vent()
+i1.do_task()
